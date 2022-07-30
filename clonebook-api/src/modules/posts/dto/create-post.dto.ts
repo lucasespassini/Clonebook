cf12ede550f4b850993fa8dad1fb734d8b76dad5
Lucas Mendes <lucasespassini@gmail.com>
2022-07-29 23:11:56 -0300

    separando backend do frontend

--- clonebook-api/src/modules/posts/dto/create-post.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class CreatePostDto {
-  @IsNotEmpty({ message: 'Digite algo para postar!' })
-  content: string;
-
-  @IsNotEmpty({ message: 'Usuário inválido!' })
-  userId: string;
-}
--- clonebook-api/src/modules/users/dto/create-user.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateUserDto {
-  user_name: string;
-  name: string;
-  email: string;
-  password: string;
-}
